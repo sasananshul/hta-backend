5fc3a052b4e99c7aeb692958b9c4f01dc114a4fa
sasananshul <sasananshul@outlook.com>
2024-05-18 16:02:06 +0530

    add: cors

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-app.use(cors);
+app.use(cors());
 
 app.get('/api/courses', getCourses);
 
