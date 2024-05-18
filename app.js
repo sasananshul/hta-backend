3e19f23f81a96a3b2cda6fdf46b9048a290a03e8
sasananshul <sasananshul@outlook.com>
2024-05-18 15:10:01 +0530

    add: cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -19,6 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+app.use(cors);
 
 app.get('/api/courses', getCourses);
 
