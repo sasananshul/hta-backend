dfd2330c994684bef1c644e0a4a09af01f636d26
sasananshul <sasananshul@outlook.com>
2024-05-18 13:19:58 +0530

    added check for filters

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -5,40 +5,39 @@ const getCourses = async (req, res) => {
         const filters = req.query;
         const filterQuery = {};
 
-        console.log('Chekpoint0')
-        if (filters.universityName.trim()) {
-            const regex = new RegExp(filters.universityName, 'i')
-            filterQuery.universityName = regex;
-        }
-        if (filters.courseName.trim()) {
-            const regex = new RegExp(filters.courseName, 'i')
-            filterQuery.courseName = regex;
-        }
-        if (filters.germanyRanking.trim()) {
-            filterQuery.germanyRanking = filters.germanyRanking;
-        }
-        if (filters.courseType) {
-            filterQuery.courseType = filters.courseType;
-        }
-        if (filters.teachingLanguage) {
-            filterQuery.teachingLanguage = filters.teachingLanguage;
-        }
-        if (filters.beginningSemester) {
-            filterQuery.beginningSemester = filters.beginningSemester;
-        }
-        if (filters.duration) {
-            filterQuery.duration = filters.duration;
-        }
-        if (filters.minFee) {
-            filterQuery.tuitionFee = { $gte: filters.minFee };
-        }
-        if (filters.maxFee) {
-            filterQuery.tuitionFee = { ...filterQuery.tuitionFee, $lte: filters.maxFee };
+        if (Object.keys(filters).length) {
+            if (filters.universityName.trim()) {
+                const regex = new RegExp(filters.universityName, 'i')
+                filterQuery.universityName = regex;
+            }
+            if (filters.courseName.trim()) {
+                const regex = new RegExp(filters.courseName, 'i')
+                filterQuery.courseName = regex;
+            }
+            if (filters.germanyRanking.trim()) {
+                filterQuery.germanyRanking = filters.germanyRanking;
+            }
+            if (filters.courseType) {
+                filterQuery.courseType = filters.courseType;
+            }
+            if (filters.teachingLanguage) {
+                filterQuery.teachingLanguage = filters.teachingLanguage;
+            }
+            if (filters.beginningSemester) {
+                filterQuery.beginningSemester = filters.beginningSemester;
+            }
+            if (filters.duration) {
+                filterQuery.duration = filters.duration;
+            }
+            if (filters.minFee) {
+                filterQuery.tuitionFee = { $gte: filters.minFee };
+            }
+            if (filters.maxFee) {
+                filterQuery.tuitionFee = { ...filterQuery.tuitionFee, $lte: filters.maxFee };
+            }
         }
 
-        console.log('Checkpoint 1')
         const courses = await Course.find(filterQuery).lean();
-        console.log('Checkpoint 2')
 
         res.json(courses).status(200);
     } catch (error) {
