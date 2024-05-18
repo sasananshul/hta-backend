74d9643433a90650f038e32db6a8238dcda38b9b
sasananshul <sasananshul@outlook.com>
2024-05-18 12:56:02 +0530

    configs

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
-app.get('/', getCourses);
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
